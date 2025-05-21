aaa8cf8e1ccb9a275a561f7b4d2decc9fdaae023
ldbarboza <lucasdaniel.b1@gmail.com>
2025-05-21 15:51:37 -0300

    first commit

---
+++ script2.py
@@ -0,0 +1,14 @@
+def data():
+    return "01/01/2024"
+
+a = 10
+b = "20"
+c = a + b
+
+def resultado(x, y):
+    return x + y
+
+def resultado(x, y):  # função duplicada
+    return x + y
+
+print("Resultado final:", c)
